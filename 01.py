25324d95b94e7d49f5a0d77ce25713fd832c7e81
jcmkk3 <45919695+jcmkk3@users.noreply.github.com>
2023-12-01 11:43:52 -0800

    Cleanup day 1

--- 01.py
+++ 01.py
@@ -1,29 +1,39 @@
 import re
 import string
+from typing import Iterable, Mapping
 
-DIGIT_NAMES = "zero one two three four five six seven eight nine".split()
+# {"0": "0", "1": "1", ...}
+digits = dict(zip(string.digits, string.digits))
 
-_lookup = dict()
-_lookup |= {name: str(i) for i, name in enumerate(DIGIT_NAMES)}
-_lookup |= {name[::-1]: str(i) for i, name in enumerate(DIGIT_NAMES)}
-_lookup |= {digit: digit for digit in string.digits}
+# {"zero": "0", "one": "1", ...}
+digits_name = dict(
+    zip("zero one two three four five six seven eight nine".split(), string.digits)
+)
 
 
-def recover_value(text: str, pattern) -> int:
+def recover_value(text: str, digits_map: Mapping[str, str]) -> int:
+    """Recover an amended calibration value.
+
+    >>> recover_value("1abc2", digits)
+    12
+
+    Merge multiple mappings for more complex criteria:
+    >>> recover_value("xtwone3four", digits | digits_name)
+    24
+
+    """
+    pattern = "|".join(digits_map.keys())
     first = re.search(pattern, text).group()
-    last = re.search(pattern, text[::-1]).group()
-
-    return int(_lookup[first] + _lookup[last])
-
-
-def solve1(lines) -> int:
-    pattern = re.compile(r"\d")
-    return sum(recover_value(line, pattern) for line in lines)
+    last = re.search(pattern[::-1], text[::-1]).group()[::-1]  # [::-1] reverses string
+    return int(digits_map[first] + digits_map[last])
 
 
-def solve2(lines) -> int:
-    pattern = re.compile(r"|".join(_lookup.keys()))
-    return sum(recover_value(line, pattern) for line in lines)
+def solve1(lines: Iterable[str]) -> int:
+    return sum(recover_value(line, digits) for line in lines)
+
+
+def solve2(lines: Iterable[str]) -> int:
+    return sum(recover_value(line, digits | digits_name) for line in lines)
 
 
 with open("input/01.txt") as f:
